b4bdb09279094b6f6be80b1fdaf36fbec9043248
ruaanls <ruanlima257@hotmail.com>
2025-05-21 17:27:00 -0300

    Validações e Exceções

--- src/main/java/br/com/fiap/challenge_mottu/Exception/ValidationException.java
+++ src/main/java/br/com/fiap/challenge_mottu/Exception/ValidationException.java
@@ -2,26 +2,28 @@ package br.com.fiap.challenge_mottu.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class ValidationException
-{
-    @ControllerAdvice
-    public class ValidationExceptionHandler {
-        @ExceptionHandler(MethodArgumentNotValidException.class)
-        public ResponseEntity<Map<String, String>>
-        handleValidationExceptions(MethodArgumentNotValidException ex) {
-            Map<String, String> errors = new HashMap<>();
-            ex.getBindingResult()
-                    .getFieldErrors()
-                    .forEach(error ->
-                            errors.put(error.getField(), error.getDefaultMessage()));
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
+@ControllerAdvice
+public class ValidationException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        // mas para ser explícito, pode-se manter o HttpStatus.BAD_REQUEST aqui também.
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
